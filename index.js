85bdd14045b8a6c18a5e01105d1adeb31783cd53
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-01 19:33:59 +0300

    feat: add gameController that handles game logic

--- index.js
+++ index.js
@@ -36,3 +36,42 @@ const Gameboard = (function() {
 const player = function(name, symbol) {
     return {name, symbol};
 };
+
+const gameController = (function(player1, player2) {
+
+    const board = Gameboard();
+    let activePlayer = player1;
+    
+    const changeActivePlayer = function() {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const getActivePlayer = () => activePlayer;
+    
+    const play = function() {
+        console.log(`It is ${controller.getActivePlayer().name}'s turn`)
+        const x = prompt("enter X");
+        const y = prompt("entet Y")
+        _place(x,y);
+        board.printBoard();
+    }
+
+    const _place = function(x,y)
+    {
+        const isCellFree = board.place(activePlayer.symbol, x,y);
+        if(isCellFree)
+            changeActivePlayer();
+        else
+            console.log("This is taken!");
+    }
+
+    return {play, getActivePlayer} 
+});
+
+const player1 = player("meow", "o");
+const player2 = player("avi", "x");
+
+const controller = gameController(player1,player2); 
+while(true) {
+    controller.play();
+}
