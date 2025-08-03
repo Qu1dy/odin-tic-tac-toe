89d6636a6147f0ad854f1778ba1dd36f761eabc1
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-03 16:08:20 +0300

    refactor(_buildBoard): use dynamic values

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const Gameboard = (function () {
     let occupiedCells;
 
     const _buildBoard = () => {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
                 board[i][j] = " ";
             }
         }
